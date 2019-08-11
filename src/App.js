0f1db845974c30d0b763495cc3759b69170b08c3
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-10 21:54:25 -0400

    First commit, working api call and displaying random quotes + authors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [quote, newQuote] = useState("");
+  const [author, newAuthor] = useState("");
+
+  useEffect(() => {
+    axios.get("https://api.quotable.io/random").then(res => {
+      const tempQuote = res.data.content;
+      const tempAuthor = res.data.author;
+      newQuote(tempQuote);
+      newAuthor(tempAuthor);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{quote}</p>
+      <p>{author}</p>
     </div>
   );
-}
+};
 
 export default App;
