80c4b95c5a88934e5afa39be7b52f7bbaf995455
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-11 10:12:09 -0400

    Added styling and cenetered content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import "./App.css";
 
@@ -6,6 +8,7 @@ const App = () => {
   const [quote, newQuote] = useState("");
   const [author, newAuthor] = useState("");
 
+  const tweetUrl = "https://twitter.com/intent/tweet";
   useEffect(() => {
     axios.get("https://api.quotable.io/random").then(res => {
       const tempQuote = res.data.content;
@@ -14,10 +17,27 @@ const App = () => {
       newAuthor(tempAuthor);
     });
   }, []);
+
+  const getNewQuote = () => {
+    axios.get("https://api.quotable.io/random").then(res => {
+      const tempQuote = res.data.content;
+      const tempAuthor = res.data.author;
+      newQuote(tempQuote);
+      newAuthor(tempAuthor);
+    });
+  };
   return (
-    <div>
-      <p>{quote}</p>
-      <p>{author}</p>
+    <div id="wrapper">
+      <h1 className="title">Random Quote App</h1>
+
+      <div id="quote-box">
+        <div id="text">
+          <p>{quote}</p>
+        </div>
+        <div id="author">
+          <h5>{author}</h5>
+        </div>
+      </div>
     </div>
   );
 };
